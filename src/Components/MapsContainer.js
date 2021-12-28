1d319ccbba5e670e85bac227be8266143a37cecb
rohan-cpi <sharma.rohan18@gmail.com>
2021-12-28 13:29:01 +0530

    Added boundary layer

--- src/Components/MapsContainer.js
+++ src/Components/MapsContainer.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import axios from "axios";
 
 mapboxgl.accessToken =
   "pk.eyJ1Ijoicm9oYW4wMTgiLCJhIjoiY2t4b25xd3V4MDFubTJ3cHRteGQ5M25ndiJ9.OZ3xYshVk4yDKWIprF6R1g";
@@ -12,6 +13,14 @@ const MapsContainer = () => {
   const [lng, setLng] = useState(77.59);
   const [lat, setLat] = useState(12.97);
   const [zoom, setZoom] = useState(9);
+  const [hoveredStateId, setHoveredStateId] = useState(null);
+  const FetchData = async () => {
+    const fetchedData = await axios.get(
+      "https://kyupid-api.vercel.app/api/areas"
+    );
+    return fetchedData.data.features[0].geometry.coordinates;
+  };
+
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -21,6 +30,9 @@ const MapsContainer = () => {
       zoom: zoom,
     });
   });
+  // Fetch data from api
+  const coordinates = FetchData();
+  console.log(coordinates);
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
@@ -29,7 +41,67 @@ const MapsContainer = () => {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
+    map.current.on("load", () => {
+      map.current.addSource("regions", {
+        type: "geojson",
+        data: "https://kyupid-api.vercel.app/api/areas",
+      });
+
+      // The feature-state dependent fill-opacity expression will render the hover effect
+      // when a feature's hover state is set to true.
+      map.current.addLayer({
+        id: "region-fills",
+        type: "fill",
+        source: "regions",
+        layout: {},
+        paint: {
+          "fill-color": "gray",
+          "fill-opacity": [
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            1,
+            0.5,
+          ],
+        },
+      });
+      map.current.addLayer({
+        id: "region-borders",
+        type: "line",
+        source: "regions",
+        layout: {},
+        paint: {
+          "line-color": "#627BC1",
+          "line-width": 2,
+        },
+      });
+      map.current.on("mousemove", "region-fills", (e) => {
+        if (e.features.length > 0) {
+          if (hoveredStateId !== null) {
+            map.current.setFeatureState(
+              { source: "regions", id: hoveredStateId },
+              { hover: false }
+            );
+          }
+          setHoveredStateId(e.features[0].id);
+          map.current.setFeatureState(
+            { source: "regions", id: hoveredStateId },
+            { hover: true }
+          );
+        }
+
+        map.current.on("mouseleave", "state-fills", () => {
+          if (hoveredStateId !== null) {
+            map.current.setFeatureState(
+              { source: "states", id: hoveredStateId },
+              { hover: false }
+            );
+          }
+          setHoveredStateId(null);
+        });
+      });
+    });
   });
+
   return (
     <div className="maps-container">
       <h1 style={{ color: "##fffffe" }}>Kyupid Analytics</h1>
